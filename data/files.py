ab09a3aed273ee3d3252bb9405ae280c9661284e
Adam <privateonly0709@gmail.com>
2026-02-20 16:33:08 +0000

    Project setup complete

---
+++ data/files.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+# manually list the files
+files = [
+    "/daily_sales_data_0.csv",
+    "/daily_sales_data_1.csv",
+    "/daily_sales_data_2.csv"
+]
+
+frames = []
+
+for file in files:
+    df = pd.read_csv(file)
+
+    # keep only Pink Morsels
+    df = df[df["product"] == "pink morsel"]
+
+    # create sales column
+    df["sales"] = df["quantity"] * df["price"]
+
+    # keep required columns
+    df = df[["sales", "date", "region"]]
+
+    frames.append(df)
+
+# combine into one dataset
+final_df = pd.concat(frames, ignore_index=True)
+
+# export result
+final_df.to_csv("formatted_sales.csv", index=False)
+
+print("formatted_sales.csv created successfully")
